8e3b6c3f8a7e686d2e2efd4a7973b1a3848eac44
zhao mac <3345@qq.com>
2019-10-09 18:01:37 +0800

    update utils

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,17 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+let lastFrameTime = 0,
+    doAnimationFrame = callback => {
+      // 模拟 requestAnimationFrame
+      let currTime = Date.now(),
+          timeToCall = Math.max(0, 16 - (currTime - lastFrameTime)),
+          id = setTimeout(() => (callback(currTime + timeToCall)), timeToCall)
+      lastFrameTime = currTime + timeToCall
+      return id
+    },
+    abortAnimationFrame = id => (clearTimeout(id))  // 模拟 cancelAnimation
+
 module.exports = {
   formatTime: formatTime
 }
